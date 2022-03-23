c7bc129caa6ba64754c5ec31154700cdcd8625aa
Shahid Ameen <43674745+iamshahidameen@users.noreply.github.com>
2022-03-23 21:45:54 +0500

    updated js to active the font icons and and image as well

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* Custom Method 1 */
 
-const mobileMenu = document.querySelectorAll('.main-menu li ');
+/*const mobileMenu = document.querySelectorAll('.main-menu li ');
 const imgEl = document.querySelectorAll('.phone img');
 
 mobileMenu.forEach((menu, menuIndex) => {
@@ -21,4 +21,28 @@ function addImgToTab(imageIndex){
             imgEl[i].classList.add('show');
         }
     }
-}  
+}  */
+
+
+//Method no 2
+const contents = document.querySelectorAll('.content')
+const listItems = document.querySelectorAll('nav ul li')
+
+listItems.forEach((item, idx) => {
+    item.addEventListener('click', () => {
+        hideAllContents()
+        hideAllItems()
+
+        item.classList.add('active')
+        contents[idx].classList.add('show')
+    })
+})
+
+function hideAllContents() {
+    contents.forEach(content => content.classList.remove('show'))
+}
+
+
+function hideAllItems() {
+    listItems.forEach(item => item.classList.remove('active'))
+}
