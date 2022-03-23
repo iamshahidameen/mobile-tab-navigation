58ca3e09c5abd6d4012de1d127d2700cfbaaf965
Shahid Ameen <43674745+iamshahidameen@users.noreply.github.com>
2022-03-23 21:41:28 +0500

    Added Simple Layout for and functionality for tabs

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-alert("Hello, World!");
+/* Custom Method 1 */
+
+const mobileMenu = document.querySelectorAll('.main-menu li ');
+const imgEl = document.querySelectorAll('.phone img');
+
+mobileMenu.forEach((menu, menuIndex) => {
+
+    menu.addEventListener('click', function() {
+        
+        addImgToTab(menuIndex);
+    });
+   
+});
+
+function addImgToTab(imageIndex){
+    
+    for(let i = 0; i<imgEl.length; i++ ) {
+        imgEl[i].classList.remove('show');
+        console.log(imageIndex, imgEl[i])
+        if(i === imageIndex) {
+            imgEl[i].classList.add('show');
+        }
+    }
+}  
